b2f4d4841818ac3e0aebbaab63e798f6b983a615
ullet <ullet@users.noreply.github.com>
2024-09-12 09:55:05 +0100

    Partially covered code

---
+++ Dummy/Library.Tests/PartiallyCoveredTests.cs
@@ -0,0 +1,36 @@
+﻿using Dummy.Library;
+
+namespace Library.Tests;
+
+public class PartiallyCoveredTests
+{
+    [Fact]
+    public void FirstCharTest()
+    {
+        Assert.Equal('W', PartiallyCovered.FirstChar("Word"));
+    }
+
+    [Fact]
+    public void ToDoubleTest()
+    {
+        Assert.Equal(1.0d, PartiallyCovered.ToDouble(1));
+    }
+
+    [Fact]
+    public void RepeatTest()
+    {
+        Assert.Equal("xxx", PartiallyCovered.Repeat('x', 3));
+    }
+
+    [Fact]
+    public void CountCharsTest()
+    {
+        Assert.Equal(7, PartiallyCovered.CountChars("1234567"));
+    }
+
+    [Fact]
+    public void ComputeBadHashTest()
+    {
+        Assert.Equal(42, PartiallyCovered.ComputeBadHash("Anything"));
+    }
+}
---
+++ Dummy/Library/PartiallyCovered.cs
@@ -0,0 +1,36 @@
+﻿namespace Dummy.Library;
+
+public static class PartiallyCovered
+{
+    public static char FirstChar(string s)
+    {
+        return s[0];
+    }
+
+    public static double ToDouble(int n)
+    {
+        return (double)n;
+    }
+
+    public static string Repeat(char c, int times)
+    {
+        return new string(c, times);
+    }
+
+    public static int CountChars(string s)
+    {
+        return s.Length;
+    }
+
+#pragma warning disable CS8603 // Possible null reference return.
+    public static TTo UnsafeCast<TFrom, TTo>(TFrom t) where TTo: class
+    {
+        return t as TTo;
+    }
+#pragma warning restore CS8603 // Possible null reference return.
+
+    public static int ComputeBadHash(object _)
+    {
+        return 42;
+    }
+}
